a846cbb87a1adb0c50d5d7be7261e16604bac123
Jeff Hykin <jeff.hykin@gmail.com>
2022-11-01 13:27:48 -0500

    -

--- main/main.rb
+++ main/main.rb
@@ -10,19 +10,8 @@ require_relative './tokens.rb'
 # 
 # 
 grammar = Grammar.fromTmLanguage("./main/modified.tmLanguage.json")
-grammar.name = ""
-# grammar = Grammar.new(
-#     name: "YOUR_LANGUAGE",
-#     scope_name: "source.jsonc",
-#     fileTypes: [
-#         "jsonc",
-#         # for example here are come C++ file extensions:
-# 		#     "cpp",
-# 		#     "cxx",
-# 		#     "c++",
-#     ],
-#     version: "",
-# )
+grammar.name = "jsonc"
+grammar.scope_name = "source.json.comments"
 
 # 
 #
@@ -60,17 +49,202 @@ grammar.name = ""
 # basic patterns
 # 
     # overwrite the stringcontent
-    grammar[:stringcontent] = [
-        valid_escape = Pattern.new(
-            tag_as: "constant.character.escape.json.comments",
-            match: Pattern.new(/\\/).then(
-                Pattern.new(/["\\\/bfnrt]/).or(/u[0-9a-fA-F]{4}/)
-            ),
+    normal_escape_tag = "constant.character.escape.json.comments"
+    outputs_just_a_backslash_tag = "entity.name.escape.backslash.insertion"
+    double_escaper_tag = "punctuation.insertion.escape.escaper comment punctuation.definition.comment.insertion.escape"
+    double_escapee_tag = "punctuation.insertion.escape.escapee string.regexp.insertion.escape"
+    normal_string_charater_tag = "string.quoted"
+    
+    grammar[:basic_escape] = Pattern.new(
+        tag_as: normal_escape_tag,
+        match: Pattern.new(/\\/).then(
+            Pattern.new(/["\\\/bfnrt]/).or(/u[0-9a-fA-F]{4}/)
         ),
-        invalid_escape = Pattern.new(
-            tag_as: "invalid.illegal.unrecognized-string-escape.json.comments",
-            match: Pattern.new(/\\\\./),
+    )
+    grammar[:invalid_escape] = Pattern.new(
+        tag_as: "invalid.illegal.unrecognized-string-escape.json.comments",
+        match: Pattern.new(/\\\\./),
+    )
+    grammar[:quad_backslash_match] = Pattern.new(
+        Pattern.new(
+            tag_as: double_escaper_tag,
+            match: /\\\\/,
+        ).then(
+            tag_as: normal_escape_tag,
+            match: /\\\\/,
+        ),
+    )
+    grammar[:null_quad_backslash] = Pattern.new(
+        # FIXME: check what \\\" does
+        match: oneOf([
+            Pattern.new(/\\\\/).zeroOrMoreOf(match: /\\\\\\\\/, no_backtrack: true).then(
+                Pattern.new(/[^\{\$"\\]/).or(lookAheadFor('"'))
+            ),
+            oneOrMoreOf(match: /\\\\\\\\/, no_backtrack: true).then(
+                Pattern.new(/[^\{\$"\\]/).or(lookAheadFor('"'))
+            ),
+        ]),
+        includes: [
+            # quad match
+            :quad_backslash_match,
+            # double match
+            Pattern.new(
+                tag_as: normal_escape_tag,
+                match: /\\\\/,
+            ),
+            :normal_characters,
+        ],
+    )
+    grammar[:dollar_sign_escape] = Pattern.new(
+        match: lookBehindToAvoid(/\\/).zeroOrMoreOf(match: /\\\\\\\\/, no_backtrack: true).then(
+            Pattern.new(/\\\\/).then("$")
+        ),
+        includes: [
+            :quad_backslash_match,
+            Pattern.new(
+                tag_as: double_escapee_tag,
+                match: Pattern.new(
+                    tag_as: double_escaper_tag,
+                    match: /\\\\/,
+                ).then("$")
+            ),
+        ]
+    )
+    grammar[:naive_bracket_escape] = Pattern.new(
+        tag_as: double_escapee_tag,
+        match: Pattern.new(
+            tag_as: double_escaper_tag,
+            match: /\\\\/,
+        ).then("}")
+    )
+    grammar[:normal_characters] = Pattern.new(
+        tag_as: normal_string_charater_tag,
+        match: /[^\\\n\}"]+/, # normal characters
+    )
+    simple_escape_context = [
+        :null_quad_backslash,
+        :dollar_sign_escape,
+        :naive_bracket_escape,
+        :basic_escape,
+        :invalid_escape,
+        :normal_characters,
+    ]
+    # assuming there is no prior escape
+    insertion_tag = "support.class.insertion"
+    grammar[:naive_insertion_area] = Pattern.new(
+        Pattern.new(
+            tag_as: insertion_tag,
+            should_fully_match: [ "$1", "$2", "$howdeee" ],
+            match: Pattern.new("$").oneOf([
+                /\d+/,
+                /[a-zA-Z_]+/, # FIXME: this probably should include more characters, maybe numbers
+            ]),
+        ).or(
+            # key question is: what results in an inert/unmatched ${}
+                # "${}"               => ${}
+                # "${:}"              => ${:}
+                # "${:a}"             => ${:a}
+                # "${0:}"             => special
+                # "${0:\\}"           => ${0:}
+                # "${0:\\a}"          => \a and special
+                # "${0:\\\\}"         => \ and special
+                # "${0:\\\\a}"        => \a and special
+                # "${0:\\\\\\}"       => ${0:\}
+                # "${0:\\\\\\\\}"     => \\ and special
+                # "${0:\\\\\\\\a}"    => \\a and special
+                # "${0:\\}\\\\\\a}"   => }\\a and special
+                # "${0:\\}\\\\\\a}"   => }\\a and special
+                # "${0:\\\\}\\\\\\a}" => \\\a} and first slash special
+                # "${0:$}"            => $ and first slash special(
+            Pattern.new(
+                tag_as: insertion_tag,
+                match: Pattern.new(
+                    Pattern.new(
+                        tag_as: "punctuation.insertion",
+                        match: /\$\{/,
+                    ).then(
+                        match: /\d+/,
+                    ).then(
+                        tag_as: "punctuation.insertion",
+                        match: /:/,
+                    )
+                ),
+            ).then(
+                zeroOrMoreOf(
+                    match: oneOf([
+                        /[^\\\n\}]/,            # a primitive character, including $
+                        /\\\\\\\\/,             # a quadruple backslash (ends up as a slash)
+                        /\\\\[^\\\n\}]/,        # a double-backslash escaped thing
+                        grammar[:basic_escape], # a normal json escape
+                    ]),
+                    includes: simple_escape_context,
+                )
+            ).then(
+                tag_as: insertion_tag + "punctuation.insertion",
+                match: "}",
+            )
         )
+    )
+    # every possible match or failed-match of insertion area
+    grammar[:any_potential_insertion] = Pattern.new(
+        Pattern.new(
+            # given the $ would be active (if no backslashes given)
+                # "$1"               => 1 is special
+                # "\\$1"             => $1
+                # "\\\\$1"           => \ and 1 is special
+                # "\\\\\\$1"         => \$1
+            # logic
+                # all quad slashes means the $ is 
+            match: oneOf([
+                # exclusively quad backslashes, meaning the insertion area is still active
+                Pattern.new(
+                    match: zeroOrMoreOf(match: /\\\\\\\\/, no_backtrack: true).then(grammar[:naive_insertion_area]),
+                    includes: [
+                        :quad_backslash_match,
+                        grammar[:naive_insertion_area],
+                    ]
+                ),
+                # double backslash, then quad backslashes, meaning the insertion area is NOT active
+                Pattern.new(
+                    match: Pattern.new(/\\\\/).zeroOrMoreOf(match: /\\\\\\\\/, no_backtrack: true).then(grammar[:naive_insertion_area]),
+                    includes: [
+                        *simple_escape_context,
+                    ]
+                ),
+            ]),
+        ).or(
+        # given the $ after the slashes would be inert
+            # 
+            # "$"                => $
+            # "$$"               => $$
+            # "\\$"              => $
+            # "\\\\$"            => \$
+            # "\\\\\\$"          => \$
+            # "\\\\\\\\$$ 1"     => \\$$ 1
+            # "$$1"              => $ and 1 is special
+            # "\\\\\\$$1"        => \$ and 1 is special
+            # "\\\\$$abra 1"     => \$ and abra is special
+            # "\\\\\\$$abra 1"   => \$ and abra is special
+            # "\\\\\\\\$$abra 1" => \\$ and abra is special
+            match: zeroOrMoreOf(match:/\\\\/, no_backtrack: true).then("$"),
+            includes: [
+                *simple_escape_context, # includes $ escape
+                # unpaired } without escapes
+                Pattern.new(
+                    tag_as: normal_string_charater_tag,
+                    match: "}",
+                ),
+            ],
+        )
+    )
+    
+    grammar[:stringcontent] = [
+        :any_potential_insertion,
+        *simple_escape_context,
+    ]
+    grammar[:string_key_content] = [
+        :basic_escape,
+        :invalid_escape,
     ]
 
 #
